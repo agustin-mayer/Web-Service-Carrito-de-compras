d4b75760f47983541c2ff053485a67a06c5a500b
mayeragustin99 <mayeragustin99@gmail.com>
2022-12-06 20:18:43 -0300

    modificacion de index

--- index.js
+++ index.js
@@ -1,48 +1,13 @@
-require('dotenv').config()
 const express = require('express')
-const path = require('path')
 const PORT = process.env.PORT || 5000
 const app = express()
-const cors = require('cors')
 
-// middleware
-app.use(express.json());
-app.use(express.urlencoded({extended: false}))
-app.use(cors())
-
-// routes
-app.use(require('./routes/index'))
 
 app.listen(PORT);
+
+app.get("/", (req,res)=> {
+  res.send("la pagina de inicio")
+})
+
 console.log('Server on port')
 
-const swaggerUiExpress = require('swagger-ui-express')
-const swaggerJsdoc = require('swagger-jsdoc')
-
-const options = {
-    definition: {
-      openapi: "3.0.0",
-      info: {
-        title: "Foodmay API",
-        version: "1.0.0",
-        description:
-          "Documentacion Foodmay API",
-        contact: {
-          name: "Agustin Mayer",
-        },
-      },
-      servers: [
-        {
-          url: "https://638fc74b717dd717a3e8da21--eloquent-medovik-1f1dd2.netlify.app/",
-        },
-      ],
-    },
-    apis: ["./routes/index.js"],
-  };
-
-  const specs = swaggerJsdoc(options);
-  app.use(
-    "/",
-    swaggerUiExpress.serve,
-    swaggerUiExpress.setup(specs)
-  );
