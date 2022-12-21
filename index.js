9797e6999f1f627e872fdcf9671144d60ae6b7d0
mayeragustin99 <mayeragustin99@gmail.com>
2022-12-21 17:48:55 -0300

    cambio de puerto

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5005
 const app = express()
 const cors = require('cors')
 const morgan = require('morgan')
