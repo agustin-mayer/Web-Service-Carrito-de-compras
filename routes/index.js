de130efb032c48dde5648bab6a4cc14c97be3b3b
mayeragustin99 <mayeragustin99@gmail.com>
2023-01-11 03:11:33 -0300

    wat

--- routes/index.js
+++ routes/index.js
@@ -9,9 +9,23 @@ const {
 } = require('../controllers/index.controller');
 
 const webpush = require('./webpush')
+let pushSubscription
 //para subscribir al usuario (escucha la primer subscripcion con el usuario)
 router.post('/subscription', async (req,res) => {
+    pushSubscription = req.body
     res.status(200).json()
+    
+    const payload = JSON.stringify({
+        title: 'My Custom Notification',
+        message: 'Hello World'
+    })
+
+    try{
+        await webpush.sendNotification(pushSubscription, payload)
+    }
+    catch (error) {
+        console.log(error)
+    }
 })
 /**
  * @swagger
