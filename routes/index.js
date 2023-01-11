26b0fd47fd777f0e12cb71b94a505f955afc633e
mayeragustin99 <mayeragustin99@gmail.com>
2023-01-11 02:56:46 -0300

    Revert "a ver si no hace nada"
    This reverts commit ae09ccf4f2989df513b73f521f303fa869f297e8.

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,20 @@ let pushSubscription
 
 //para subscribir al usuario (escucha la primer subscripcion con el usuario)
 router.post('/subscription', async (req,res) => {
-   
+    pushSubscription = req.body
+    res.status(200).json()
+    
+    const payload = JSON.stringify({
+        title: 'My Custom Notification',
+        message: 'Hello World'
+    })
+
+    try{
+        await webpush.sendNotification(pushSubscription, payload)
+    }
+    catch (error) {
+        console.log(error)
+    }
 })
 /**
  * @swagger
