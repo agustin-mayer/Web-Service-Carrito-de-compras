9eb0b481a1d34f93c2203b4269b207fb248ce599
mayeragustin99 <mayeragustin99@gmail.com>
2023-01-31 03:07:02 -0300

    a ver sin cors

--- index.js
+++ index.js
@@ -3,13 +3,11 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const app = express()
-const cors = require('cors')
 const morgan = require('morgan')
 
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: false}))
-app.use(cors())
 app.use(morgan('dev'))
 
 // routes
